2c9310a6f08dd093d9313bb54da85ee70cad76ee
bbundy12 <brendanbundy1@gmail.com>
2024-03-01 20:03:25 -0700

    WE ARE DONE!!!!

--- Mission08_3-12/Models/EFTaskRepository.cs
+++ Mission08_3-12/Models/EFTaskRepository.cs
@@ -18,6 +18,11 @@ namespace Mission08_3_12.Models
             _context.Tasks.Add(task);
             _context.SaveChanges();
         }
+        public void UpdateSingleTask(TaskFix task)
+        {
+            _context.Tasks.Update(task);
+            _context.SaveChanges();
+        }
         public void RemoveSingleTask(TaskFix task)
         {
             _context.Tasks.Remove(task);
